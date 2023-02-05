1d19890d84dace8a942c8647814054829cc20603
Artem Karpenko <dante120591@gmail.com>
2023-02-05 11:38:40 +0200

    Iteration 1

---
+++ app.js
@@ -0,0 +1,34 @@
+const cors = require("cors");
+const mongoose = require("mongoose");
+const express = require("express");
+
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+const http = require("http").Server(app);
+
+const socket = require("socket.io")(http);
+
+global.onlineUsers = new Map();
+
+socket.on("connection", (user) => {
+  user.emit("changeOnline", onlineUsers.size);
+  console.log("Connected");
+  user.broadcast.emit("changeOnline", onlineUsers.size + 1);
+  user.on("addUser", (data) => {
+    onlineUsers.set(user.id, data.name);
+    user.emit("changeOnline", onlineUsers.size);
+  });
+  user.on("newMessage", (message) => {
+    user.broadcast.emit("showMessage", message);
+  });
+  user.on("disconnect", () => {
+    onlineUsers.delete(user.id);
+    user.broadcast.emit("changeOnline", onlineUsers.size);
+  });
+});
+
+const { PORT } = process.env;
+
+http.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
